99673ca6c999fb286d5554f07bb7aa2f47d486a1
Daniel Budiansky <daniel.budiansky@protonmail.com>
2026-01-18 05:21:44 +0100

    feat(m0): add initial recipe list screen scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
       home: const RecipeListPage(title: 'Recipes'),
     );
   }
