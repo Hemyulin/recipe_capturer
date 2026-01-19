23a1a1e887b59d2ca4d6705ec3af80c18bdfb537
Daniel Budiansky <daniel.budiansky@protonmail.com>
2026-01-19 14:16:05 +0100

    fix/commit

--- test/domain/recipe_test.dart
+++ test/domain/recipe_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:recipe_capturer/main.dart';
 
 void main() {
-  testWidgets('description', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-  });
+  test('Recipe.create trims title and allows empty string', () {});
 }
