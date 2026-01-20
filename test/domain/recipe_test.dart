272e0b691ceb7219c977b39bdf5eb4e8730faffa
Daniel Budiansky <daniel.budiansky@protonmail.com>
2026-01-20 10:44:48 +0100

    working on recipe domain shape

--- test/domain/recipe_test.dart
+++ test/domain/recipe_test.dart
@@ -1,5 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe_capturer/domain/recipe.dart';
 
 void main() {
-  test('Recipe.create trims title and allows empty string', () {});
+  test('Recipe.create trims title', () {
+    final recipe = Recipe.create('  test title       ');
+    expect(recipe.title, 'test title');
+  });
+
+  test('Recipe.create allows empty string', () {
+    final recipe = Recipe.create('');
+    expect(recipe.title, '');
+  });
+
+  //   test('Recipe.create contains creation timestamp', () {
+  //     DateTime fixed = DateTime(2026, 1, 1);
+
+  //     final recipe = Recipe.create('x', now: fixed);
+  //     expect(recipe.createdAt, fixed);
+  //   });
 }
